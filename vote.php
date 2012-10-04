a795917368ee030f3b7a8cfce80f318d433efad3
Jeremy Harris <jharris@rockharbor.org>
2012-10-04 11:41:33 -0700

    Not updating invalid polls

--- vote.php
+++ vote.php
@@ -5,7 +5,11 @@ if (empty($_POST) || empty($_POST['question'])) {
 	redirect('404');
 }
 
-$response = array();
+$response = array(
+	'success' => false,
+	'double' => false,
+	'message' => 'Something went wrong when trying to process your vote.'
+);
 
 // get existing votes
 $votes = array();
@@ -19,36 +23,56 @@ if ($votes[$_POST['question']]) {
 		'double' => true,
 		'message' => 'You already voted for that question!'
 	);
-} else {
-	$config = json_decode(file_get_contents('config.json'));
+	echo json_encode($response);
+	return;
+}
+$config = json_decode(file_get_contents('config.json'));
 
-	try {
-		$connection = new PDO(
-			"sqlite:$config->database",
-			null,
-			null,
-			array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-		);
-	} catch (PDOException $e) {
-		debug($e);
-	}
-	
-	$query = $connection->prepare("UPDATE `questions` SET `votes` = (`votes` + 1) WHERE `rowid` = {$_POST['question']};");
-	if ($query->execute()) {
-		$response = array(
-			'success' => true,
-			'double' => false,
-			'message' => 'Thanks for voting!'
-		);
-		$votes[$_POST['question']] = true;
-		setcookie('votes', serialize($votes), strtotime('+20 years'));
-	} else {
-		$response = array(
-			'success' => false,
-			'double' => false,
-			'message' => 'Something went wrong when trying to process your vote.'
-		);
-	}
+try {
+	$connection = new PDO(
+		"sqlite:$config->database",
+		null,
+		null,
+		array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+	);
+} catch (PDOException $e) {
+	debug($e);
+}
+
+// make sure this is a valid poll
+$query = $connection->prepare("SELECT `poll_id` FROM `questions` WHERE `rowid` = {$_POST['question']} LIMIT 1;");
+if (!$query->execute()) {
+	echo json_encode($response);
+	return;
+}
+
+$question = $query->fetchObject();
+
+$query = $connection->prepare("SELECT * FROM `polls` WHERE `rowid` = $question->poll_id LIMIT 1;");
+if (!$query->execute()) {
+	echo json_encode($response);
+	return;
+}
+
+$poll = $query->fetchObject();
+$expired = $poll->expires && (strtotime($poll->expires) - strtotime() <= 0);
+if (!$poll->enabled || $expired) {
+	$response['message'] = 'Invalid poll!';
+	echo json_encode($response);
+	return;
+}
+
+$query = $connection->prepare("UPDATE `questions` SET `votes` = (`votes` + 1) WHERE `rowid` = {$_POST['question']};");
+if ($query->execute()) {
+	$response = array(
+		'success' => true,
+		'double' => false,
+		'message' => 'Thanks for voting!'
+	);
+	$votes[$_POST['question']] = true;
+	setcookie('votes', serialize($votes), strtotime('+20 years'));
+} else {
+	$response['message'] = 'Something went wrong when trying to process your vote.';
 }
 
 echo json_encode($response);
